0247ca1769697020258b4e2cb0173f50bef5f76f
zzangmmz <leemj036@gmail.com>
2025-04-08 17:30:38 +0900

    [refactor] 뷰와 뷰컨트롤러 분리

--- ConDemo/View/LaunchView.swift
+++ ConDemo/View/LaunchView.swift
@@ -1,5 +1,5 @@
 //
-//  RecordingMainView.swift
+//  LaunchView.swift
 //  ConDemo
 //
 //  Created by 이명지 on 4/8/25.
@@ -7,29 +7,21 @@
 
 import UIKit
 
-final class RecordingMainView: UIView {
+final class LaunchView: UIView {
     // MARK: - Properties
 
-    private(set) var startButton: CircleButton = .init(title: "Start")
-
-    private(set) var historyButton: CircleButton = .init(title: "History")
-
-    private(set) var communityButton: CircleButton = .init(title: "Community")
+    private(set) var launchLogoImageView: UIImageView = {
+        let imageView: UIImageView = .init(image: UIImage(named: "LaunchLogo"))
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
 
     private var adBannerImageView: UIImageView = {
         let imageView: UIImageView = .init()
         imageView.contentMode = .scaleAspectFit
         imageView.backgroundColor = .beigeGray
         imageView.tintColor = .black
-        imageView.layer.cornerRadius = 14
         return imageView
-    }()
-
-    private lazy var bottomButtonStackView = {
-        let stackView: UIStackView = .init(arrangedSubviews: [historyButton, communityButton])
-        stackView.axis = .horizontal
-        stackView.spacing = 21
-        return stackView
     }()
 
     private var copyrightLabel: UILabel = {
@@ -45,7 +37,7 @@ final class RecordingMainView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-        setupView()
+        setupLaunchScreen()
         setupSubviews()
         setupConstraints()
     }
@@ -57,48 +49,34 @@ final class RecordingMainView: UIView {
 
     // MARK: - Functions
 
-    private func setupView() {
+    private func setupLaunchScreen() {
         backgroundColor = .white
     }
 
     private func setupSubviews() {
-        [adBannerImageView,
-         startButton,
-         bottomButtonStackView,
+        [launchLogoImageView,
+         adBannerImageView,
          copyrightLabel].forEach {
             self.addSubview($0)
         }
     }
 
     private func setupConstraints() {
-        adBannerImageView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(96)
-            make.horizontalEdges.equalToSuperview().inset(13)
-            make.height.equalTo(72)
-        }
-
-        startButton.snp.makeConstraints { make in
+        launchLogoImageView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
-            make.size.equalTo(212)
-        }
-
-        historyButton.snp.makeConstraints { make in
-            make.size.equalTo(98)
-        }
-
-        communityButton.snp.makeConstraints { make in
-            make.size.equalTo(98)
-        }
-
-        bottomButtonStackView.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(startButton.snp.bottom).offset(55)
+            make.size.equalTo(130)
         }
 
         copyrightLabel.snp.makeConstraints { make in
             make.bottom.equalToSuperview().offset(-34)
             make.centerX.equalToSuperview()
+        }
+
+        adBannerImageView.snp.makeConstraints { make in
+            make.top.equalTo(launchLogoImageView.snp.bottom).offset(30)
+            make.horizontalEdges.equalToSuperview().inset(13)
+            make.bottom.equalTo(copyrightLabel.snp.top).offset(-30)
         }
     }
 }
--- ConDemo/ViewController/LaunchViewController.swift
+++ ConDemo/ViewController/LaunchViewController.swift
@@ -11,35 +11,13 @@ import UIKit
 final class LaunchViewController: UIViewController {
     // MARK: - Properties
 
-    private var launchLogoImageView: UIImageView = {
-        let imageView: UIImageView = .init(image: UIImage(named: "LaunchLogo"))
-        imageView.contentMode = .scaleAspectFit
-        return imageView
-    }()
-
-    private var adBannerImageView: UIImageView = {
-        let imageView: UIImageView = .init()
-        imageView.contentMode = .scaleAspectFit
-        imageView.backgroundColor = .beigeGray
-        imageView.tintColor = .black
-        return imageView
-    }()
-
-    private var copyrightLabel: UILabel = {
-        let label: UILabel = .init()
-        label.text = "Copyright © 2025 Ourvoices. All Rights Reserved"
-        label.font = UIFont(name: "Pretendard-Medium", size: 7)
-        label.textColor = .black
-        return label
-    }()
+    private var launchView: LaunchView = .init()
 
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupLaunchScreen()
-        setupSubviews()
-        setupConstraints()
+        view = launchView
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -49,40 +27,9 @@ final class LaunchViewController: UIViewController {
 
     // MARK: - Functions
 
-    private func setupLaunchScreen() {
-        view.backgroundColor = .white
-    }
-
-    private func setupSubviews() {
-        [launchLogoImageView,
-         adBannerImageView,
-         copyrightLabel].forEach {
-            self.view.addSubview($0)
-        }
-    }
-
-    private func setupConstraints() {
-        launchLogoImageView.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview()
-            make.size.equalTo(130)
-        }
-
-        copyrightLabel.snp.makeConstraints { make in
-            make.bottom.equalToSuperview().offset(-34)
-            make.centerX.equalToSuperview()
-        }
-
-        adBannerImageView.snp.makeConstraints { make in
-            make.top.equalTo(launchLogoImageView.snp.bottom).offset(30)
-            make.horizontalEdges.equalToSuperview().inset(13)
-            make.bottom.equalTo(copyrightLabel.snp.top).offset(-30)
-        }
-    }
-
     private func animateLaunchLogo() {
         UIView.animate(withDuration: 1.0, delay: 0.5, options: .curveEaseInOut, animations: {
-            self.launchLogoImageView.transform = CGAffineTransform(rotationAngle: .pi)
+            self.launchView.launchLogoImageView.transform = CGAffineTransform(rotationAngle: .pi)
         }, completion: { _ in
             self.fadeOutLogo()
         })
@@ -90,7 +37,7 @@ final class LaunchViewController: UIViewController {
 
     private func fadeOutLogo() {
         UIView.animate(withDuration: 0.8, animations: {
-            self.launchLogoImageView.alpha = 0
+            self.launchView.launchLogoImageView.alpha = 0
         }, completion: { _ in
             self.navigateToMain()
         })
