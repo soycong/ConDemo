b8bebf8464648178e75839f4cf0a2569a046c86b
zzangmmz <leemj036@gmail.com>
2025-04-09 12:56:01 +0900

    [feat] 한국 시간으로 변환하는 toKoreaFormat() 메서드 추가

---
+++ ConDemo/Extension/+Date.swift
@@ -0,0 +1,17 @@
+//
+//  +Date.swift
+//  ConDemo
+//
+//  Created by 이명지 on 4/9/25.
+//
+
+import Foundation
+
+extension Date {
+    func toKoreaFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateFormat = "yyyy.MM.dd a HH:mm"
+        return dateFormatter.string(from: self)
+    }
+}
