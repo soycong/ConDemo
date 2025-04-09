5d40d07a8ba1e566146e7e8379e71e0284476362
soy <soysauce1223@naver.com>
2025-04-09 14:55:59 +0900

    [feat] systemIcon 이름을 모아놓은 구조체 선언

---
+++ ConDemo/ButtonSystemIcon.swift
@@ -0,0 +1,16 @@
+//
+//  ButtonSystemIcon.swift
+//  ConDemo
+//
+//  Created by seohuibaek on 4/9/25.
+//
+
+struct ButtonSystemIcon {
+    static let shareButtonImage = "square.and.arrow.up"
+    static let websiteButtonImage = "safari"
+
+    static let upButtonImage = "chevron.up"
+    static let downButtonImage = "chevron.down"
+    
+    static let cancelButtonImage = "xmark"
+}
