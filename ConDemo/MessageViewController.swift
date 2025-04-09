f40068e29ee6f49bdd99fde72ea8ae49f8788cf8
soy <soysauce1223@naver.com>
2025-04-10 00:44:28 +0900

    [feat] 채팅 상단 네비게이션 바 구현

--- ConDemo/MessageViewController.swift
+++ ConDemo/MessageViewController.swift
@@ -14,5 +14,29 @@ final class MessageViewController: UIViewController {
         super.viewDidLoad()
         
         self.view = messageView
+        
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.tintColor = .label
+        navigationItem.title = .none
+                
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"),
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(backButtonTapped))
+        
+        let waveButton = UIBarButtonItem(image: UIImage(systemName: "waveform"),
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(backButtonTapped))
+        
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.rightBarButtonItem = waveButton
+    }
+    
+    @objc private func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
     }
 }
