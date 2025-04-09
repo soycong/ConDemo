880d88faf9ba05480e7b536886e311d346efd6a6
zzangmmz <leemj036@gmail.com>
2025-04-09 11:39:10 +0900

    [feat] 네비게이션바 커스텀

--- ConDemo/ViewController/RecordingMainViewController.swift
+++ ConDemo/ViewController/RecordingMainViewController.swift
@@ -8,10 +8,44 @@
 import UIKit
 
 final class RecordingMainViewController: UIViewController {
+    // MARK: - Properties
+
     private let recordingMainView: RecordingMainView = .init()
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view = recordingMainView
+        setNavigationBar()
     }
+
+    // MARK: - Functions
+
+    private func setNavigationBar() {
+        navigationController?.navigationBar.tintColor = .label
+        navigationItem.title = .none
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: .init(systemName: "chevron.left"),
+                                                           style: .plain, target: self,
+                                                           action: #selector(backButtonTapped))
+        navigationItem
+            .rightBarButtonItems =
+            [UIBarButtonItem(image: .init(systemName: "person.circle"),
+                             style: .plain, target: self,
+                             action: #selector(profileButtonTapped)),
+             UIBarButtonItem(image: .init(systemName: "calendar"), style: .plain, target: self,
+                             action: #selector(calendarButtonTapped))]
+    }
+
+    @objc
+    private func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+
+    @objc
+    private func calendarButtonTapped() { }
+
+    @objc
+    private func profileButtonTapped() { }
 }
