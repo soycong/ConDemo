5b70c4e2220a3cec6bd4ee3b0d86f8565568f919
zzangmmz <leemj036@gmail.com>
2025-04-09 10:39:59 +0900

    [feat] 시작 버튼 이벤트 설정

--- ConDemo/ViewController/RecordingLandingViewController.swift
+++ ConDemo/ViewController/RecordingLandingViewController.swift
@@ -11,12 +11,22 @@ import UIKit
 final class RecordingLandingViewController: UIViewController {
     // MARK: - Properties
 
-    private var recordingLandingView: RecordingLandingView = .init()
+    private let recordingLandingView: RecordingLandingView = .init()
+    private let recordingMainViewController: RecordingMainViewController = .init()
 
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view = recordingLandingView
+        setAddTargets()
+    }
+    
+    private func setAddTargets() {
+        recordingLandingView.startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func startButtonTapped() {
+        self.navigationController?.pushViewController(recordingMainViewController, animated: true)
     }
 }
