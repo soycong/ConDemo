ef4a9aec4922648d558a8c793bb59dbc105bb90e
soy <soysauce1223@naver.com>
2025-04-10 11:06:26 +0900

    [feat] 카메라, 앨범에서 이미지를 선택할 수 있는 기능 추가

--- ConDemo/MessageViewController.swift
+++ ConDemo/MessageViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import PhotosUI
 
 final class MessageViewController: UIViewController {
     private let messageView = MessageView()
+    
+    private var picker: PHPickerViewController?
+    private var camera = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +20,9 @@ final class MessageViewController: UIViewController {
         self.view = messageView
         
         setupNavigationBar()
+        setupPHPicker()
+        setupImagePicker()
+        
         setupActions()
     }
     
@@ -41,6 +48,20 @@ final class MessageViewController: UIViewController {
         messageView.addButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
     }
     
+    private func setupPHPicker() {
+        var config = PHPickerConfiguration()
+        config.selectionLimit = 1
+        config.filter = .images
+        
+        picker = PHPickerViewController(configuration: config)
+        picker?.delegate = self
+    }
+    
+    private func setupImagePicker() {
+        camera.sourceType = .camera
+        camera.delegate = self
+    }
+    
     @objc private func backButtonTapped() {
         navigationController?.popViewController(animated: true)
     }
@@ -51,16 +72,18 @@ final class MessageViewController: UIViewController {
     }
     
     private func showAddOptions() {
-        let alertController = UIAlertController(
-        title: nil,
-        message: nil,
-        preferredStyle: .actionSheet
-    )
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         
         let cameraAction = UIAlertAction(title: "카메라", style: .default) { [weak self] _ in
+            if let camera = self?.camera {
+                self?.present(camera, animated: true)
+            }
         }
         
         let photoLibraryAction = UIAlertAction(title: "앨범에서 선택", style: .default) { [weak self] _ in
+            if let picker = self?.picker {
+                self?.present(picker, animated: true)
+            }
         }
         
         let recordingAction = UIAlertAction(title: "녹음 앱에서 선택", style: .default) { [weak self] _ in
@@ -74,5 +97,38 @@ final class MessageViewController: UIViewController {
         alertController.addAction(cancelAction)
 
         present(alertController, animated: true)
+    }
+}
+
+extension MessageViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        if let itemProvider = results.first?.itemProvider,
+           itemProvider.canLoadObject(ofClass: UIImage.self){
+            itemProvider.loadObject(ofClass: UIImage.self) { image, error in
+                if let image = image as? UIImage {
+                    DispatchQueue.main.async {
+                        self.messageView.sendImage(image: image)
+                    }
+                }
+                
+            }
+        }
+    }
+}
+
+extension MessageViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.originalImage] as? UIImage {
+            messageView.sendImage(image: image)
+        }
+        
+        picker.dismiss(animated: true)
     }
 }
