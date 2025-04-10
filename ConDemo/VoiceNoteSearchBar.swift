48d6c515dda6eaf019d5999e35c42f702f92299e
soy <soysauce1223@naver.com>
2025-04-11 02:13:43 +0900

    [fix] 서치바 배경색 바뀌지 않던 문제 수정

--- ConDemo/VoiceNoteSearchBar.swift
+++ ConDemo/VoiceNoteSearchBar.swift
@@ -12,15 +12,24 @@ final class VoiceNoteSearchBar: UISearchBar {
     init() {
         super.init(frame: .zero)
 
-        searchBarStyle = .minimal
+        searchBarStyle = .default
+        
+        backgroundImage = UIImage() // 위, 아래 줄 제거
+        
+        // 테두리 없애기 (추가 조치)
+        // setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+        
+        // setSearchFieldBackgroundImage(UIImage(), for: .normal)
 
         searchTextField.backgroundColor = .backgroundGray
-        searchTextField.layer.cornerRadius = 20
+        searchTextField.layer.cornerRadius = 18
         searchTextField.clipsToBounds = true
         
         searchTextField.layer.borderWidth = 1.0
-        searchTextField.layer.borderColor = UIColor.gray.cgColor
+        searchTextField.layer.borderColor = UIColor.black.cgColor
         
+        searchTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+
         placeholder = "대화 내용 검색"
     }
     
