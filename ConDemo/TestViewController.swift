bf44b1dc872d7bdeea1c709271f8b0944f047b8f
zzangmmz <leemj036@gmail.com>
2025-04-11 11:46:25 +0900

    [refactor] 바텀시트 RecordingMainViewController로 분리

--- ConDemo/TestViewController.swift
+++ ConDemo/TestViewController.swift
@@ -9,21 +9,10 @@ import UIKit
 
 class TestViewController: UIViewController {
     
-    private var didPresentSheet = false
-
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupButtons()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        if !didPresentSheet {
-            presentAsBottomSheet(VoiceNoteViewController())
-            didPresentSheet = true
-        }
     }
 
     func setupButtons() {
@@ -78,34 +67,6 @@ class TestViewController: UIViewController {
             return
         }
         
-        didPresentSheet = false
         navigationController?.pushViewController(destinationVC, animated: true)
-    }
-    
-    func presentAsBottomSheet(_ viewController: UIViewController) {
-        // 시트 프레젠테이션 컨트롤러 구성
-        let customIdentifier = UISheetPresentationController.Detent.Identifier("custom20")
-        let customDetent = UISheetPresentationController.Detent.custom(identifier: customIdentifier) { context in
-            return 20 // 원하는 높이
-        }
-        
-        if let sheet = viewController.sheetPresentationController {
-            sheet.detents = [
-                customDetent,
-                .medium(), // 화면 중간 높이까지
-                .large()   // 전체 화면 높이
-            ]
-            
-            // 사용자가 시트를 끌어올릴 수 있도록 설정
-            sheet.prefersGrabberVisible = true
-            
-            // 시작 시 어떤 높이로 표시할지 설정
-            sheet.selectedDetentIdentifier = .some(customIdentifier)
-
-            // 드래그 중에 아래 뷰가 어두워지지 않도록 설정
-            sheet.largestUndimmedDetentIdentifier = .large
-        }
-        
-        present(viewController, animated: true)
     }
 }
