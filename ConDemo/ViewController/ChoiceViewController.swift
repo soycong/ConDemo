60439c7e47059eae9312340585efe5adeb764e2f
zzangmmz <leemj036@gmail.com>
2025-04-21 14:10:08 +0900

    [feat] 화자 선택 뷰 생성

---
+++ ConDemo/ViewController/ChoiceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ChoiceViewController.swift
+//  ConDemo
+//
+//  Created by 이명지 on 4/21/25.
+//
+
+import UIKit
+
+final class ChoiceViewController: UIViewController {
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+}
