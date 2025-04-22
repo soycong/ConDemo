cd0f4d1b792fc7a6ce9d5de3032119d0df08ba1f
soy <soysauce1223@naver.com>
2025-04-22 11:45:11 +0900

    [refactor] 새로운 SpeechRecognizer 적용을 위한 코드 리팩토링

--- ConDemo/ViewController/RecordingScriptViewController.swift
+++ ConDemo/ViewController/RecordingScriptViewController.swift
@@ -23,20 +23,7 @@ final class RecordingScriptViewController: UIViewController, TranscriptionDelega
     }
 
     // TranscriptionDelegate 메서드 구현
-    func didReceiveTranscription(text: String, speaker: String) {
-        // 마침표를 기준으로 줄바꿈 적용
-        let formattedText = text.replacingOccurrences(of: " \\. ",
-                                                     with: ".\n",
-                                                     options: .regularExpression)
-                                .replacingOccurrences(of: " \\? ",
-                                                     with: "?\n",
-                                                     options: .regularExpression)
-        
-        let isCurrentUser = speaker == "0"
-        let newMessage = MessageData(text: formattedText, isFromCurrentUser: isCurrentUser)
-        
-        // 기존 메시지에 추가하거나 새로운 메시지로 설정
-        // viewModel.addMessage(newMessage)
-        recordingScriptView.updateTextView(newMessage.text)
+    func didReceiveTranscription(text: String) {
+        recordingScriptView.updateTextView(text)
     }
 }
