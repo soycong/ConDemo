62c2ced2248c29b9ebdc0a47ebb763ff765beb4f
zzangmmz <leemj036@gmail.com>
2025-04-23 17:57:55 +0900

    [feat] ChatGPT 응답값으로 받은 데이터 dotChart에 표시할 뷰 모델 구현

--- ConDemo/ViewModel/DotRatingViewModel.swift
+++ ConDemo/ViewModel/DotRatingViewModel.swift
@@ -8,23 +8,47 @@
 import UIKit
 
 final class DotRatingViewModel {
-    private var ratingData: DotRatingData
+    private var consistencyData: ConsistencyData?
+    private var factualAccuracyData: FactualAccuracyData?
+    private let title: String
+    private let ratingLabels: [String]
     
-    var title: String { ratingData.title }
+    var chartTitle: String { title }
     
-    var myRating: Int { ratingData.myRating }
+    var myRating: Int {
+        if title == "주장의 일관성" {
+            return consistencyData?.speakerA.score ?? 0
+        } else {
+            return factualAccuracyData?.speakerA.score ?? 0
+        }
+    }
     
-    var yourRating: Int { ratingData.yourRating }
+    var yourRating: Int {
+        if title == "주장의 일관성" {
+            return consistencyData?.speakerB.score ?? 0
+        } else {
+            return factualAccuracyData?.speakerB.score ?? 0
+        }
+    }
     
-    var labels: [String] { ratingData.labels }
+    var ratingDescriptions: [String] {
+        return ratingLabels
+    }
     
-    init(ratingData: DotRatingData) {
-        self.ratingData = ratingData
+    init(title: String, consistencyData: ConsistencyData? = nil, factualAccuracyData: FactualAccuracyData? = nil, ratingLabels: [String]) {
+        self.title = title
+        self.consistencyData = consistencyData
+        self.factualAccuracyData = factualAccuracyData
+        self.ratingLabels = ratingLabels
     }
 }
 
 extension DotRatingViewModel {
-    func updateData(_ ratingData: DotRatingData) {
-        self.ratingData = ratingData
+    func configure(with consistencyData: ConsistencyData) {
+        self.consistencyData = consistencyData
+    }
+    
+    func configure(with factualAccuracyData: FactualAccuracyData) {
+        self.factualAccuracyData = factualAccuracyData
     }
 }
