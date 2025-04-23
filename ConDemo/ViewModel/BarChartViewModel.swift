2972597f7f21c20c6ee19b8dba40457016105a8c
zzangmmz <leemj036@gmail.com>
2025-04-23 12:17:11 +0900

    [feat] 긍정/부정 데이터 구별하는 Bool값 파라미터로 추가, 표시할 spaekerA/B 데이터 추가

--- ConDemo/ViewModel/BarChartViewModel.swift
+++ ConDemo/ViewModel/BarChartViewModel.swift
@@ -11,16 +11,22 @@ import UIKit
 final class BarChartViewModel {
     private var barData: SentimentAnalysisData
     private let title: String
+    let isPositive: Bool
     
     var chartTitle: String { title }
+    
+    var myValue: SentimentExamplesData { barData.speakerA }
+    
+    var yourValue: SentimentExamplesData { barData.speakerB }
 
     var myColor: UIColor { UIColor(red: 170/255, green: 0/255, blue: 0/255, alpha: 1) }
     
     var yourColor: UIColor { UIColor(red: 220/255, green: 25/255, blue: 25/255, alpha: 1) }
     
-    init(barData: SentimentAnalysisData, title: String) {
+    init(barData: SentimentAnalysisData, title: String, isPositive: Bool) {
         self.barData = barData
         self.title = title
+        self.isPositive = isPositive
     }
     
     func updateData(_ barData: SentimentAnalysisData) {
