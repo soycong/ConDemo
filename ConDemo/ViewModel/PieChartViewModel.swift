406c559f32607523bf3a6adcf64f8bbae48ed30b
zzangmmz <leemj036@gmail.com>
2025-04-23 11:26:58 +0900

    [feat] 원형 그래프 뷰모델 생성

---
+++ ConDemo/ViewModel/PieChartViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  PieChartViewModel.swift
+//  ConDemo
+//
+//  Created by 이명지 on 4/23/25.
+//
+
+import DGCharts
+
+final class PieChartViewModel {
+    private let title: String
+    
+    var chartTitle: String {
+        
+    }
+    
+    var chartSubtitle: String {
+        
+    }
+    
+    var chartEntries: [PieChartDataEntry] {
+        return [
+            
+        ]
+    }
+    
+    init(title: String) {
+        self.title = title
+    }
+}
