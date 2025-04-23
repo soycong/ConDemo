ade90700319e27fb0e4801e2cc6d20b7c5524e01
zzangmmz <leemj036@gmail.com>
2025-04-23 16:24:58 +0900

    [feat] dot 레이팅 차트 데이터 생성

---
+++ ConDemo/Model/DotRatingData.swift
@@ -0,0 +1,13 @@
+//
+//  DotRatingData.swift
+//  ConDemo
+//
+//  Created by 이명지 on 4/23/25.
+//
+
+struct DotRatingData {
+    let title: String
+    let speakerARating: Int // 1-5 사이 값
+    let speakerBRating: Int // 1-5 사이 값
+    let labels: [String] // 각 점수별 레이블
+}
