2ddb6512f4b83f593342843d6fb5fe774649074c
zzangmmz <leemj036@gmail.com>
2025-04-23 12:32:48 +0900

    [feat] 파이 차트 데이터 추가

--- ConDemo/ViewModel/PieChartViewModel.swift
+++ ConDemo/ViewModel/PieChartViewModel.swift
@@ -6,25 +6,34 @@
 //
 
 import DGCharts
+import UIKit
 
 final class PieChartViewModel {
-    private let title: String
+    private var pieData: SpeakingTimeData
     
-    var chartTitle: String {
-        
-    }
-    
-    var chartSubtitle: String {
-        
-    }
+    var chartSubtitle: String { "총 " + String(Int(pieData.speakerA + pieData.speakerB)) + "분"}
     
     var chartEntries: [PieChartDataEntry] {
         return [
-            
+            PieChartDataEntry(value: pieData.speakerA, label: "Me"),
+            PieChartDataEntry(value: pieData.speakerB, label: "You")
         ]
     }
     
-    init(title: String) {
-        self.title = title
+    var chartColors: [UIColor] {
+        [
+            UIColor(red: 170/255, green: 0/255, blue: 0/255, alpha: 1),
+            UIColor(red: 220/255, green: 25/255, blue: 25/255, alpha: 1)
+        ]
+    }
+    
+    init(pieData: SpeakingTimeData) {
+        self.pieData = pieData
+    }
+}
+
+extension PieChartViewModel {
+    func updateData(_ pieData: SpeakingTimeData) {
+        self.pieData = pieData
     }
 }
